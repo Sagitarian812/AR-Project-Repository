e1b87f3c4bd707b9c79af78067b7e94ce2cd0ad3
Sagitarian812 <62075331+Sagitarian812@users.noreply.github.com>
2020-10-17 13:44:07 -0400

    Work

---
+++ AR_Project/Assets/Turret.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    public Transform target;
+    public float range = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void UpdateTarget()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
