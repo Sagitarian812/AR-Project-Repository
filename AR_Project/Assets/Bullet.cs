908254c23fbae90e17684de69cb0f02c74b9040a
Sagitarian812 <62075331+Sagitarian812@users.noreply.github.com>
2020-10-17 14:50:47 -0400

    Finished Bullet &Particle effect

--- AR_Project/Assets/Bullet.cs
+++ AR_Project/Assets/Bullet.cs
@@ -6,6 +6,9 @@ public class Bullet : MonoBehaviour
 {
 
     private Transform target;
+    public GameObject impactEffect;
+
+    public float speed = 70f;
 
     public void Seek(Transform _target)
     {
@@ -15,6 +18,29 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <=distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    void HitTarget()
+    {
+        GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, 2f);
+        Destroy(gameObject);
     }
 }
