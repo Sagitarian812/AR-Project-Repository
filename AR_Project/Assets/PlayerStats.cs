d43d364a72ea6f8581dcd216085dd29d08cbe2b5
Sagitarian812 <62075331+Sagitarian812@users.noreply.github.com>
2020-10-18 16:11:37 -0400

    Start lives

---
+++ AR_Project/Assets/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public static int Lives;
+    public int startLives = 20;
+
+    void Start()
+    {
+        Lives = startLives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
